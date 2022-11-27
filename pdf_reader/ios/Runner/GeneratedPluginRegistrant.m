fa82f38dfea7cfb455a09432d872c42e7c64b451
Nhattai0897 <94519492+Nhattai0897@users.noreply.github.com>
2022-11-27 16:33:00 +0700

    test branch
    Storage Permission test

---
+++ pdf_reader/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,89 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<disk_space/SwiftDiskSpacePlugin.h>)
+#import <disk_space/SwiftDiskSpacePlugin.h>
+#else
+@import disk_space;
+#endif
+
+#if __has_include(<file_picker/FilePickerPlugin.h>)
+#import <file_picker/FilePickerPlugin.h>
+#else
+@import file_picker;
+#endif
+
+#if __has_include(<flutter_fullpdfview/FlutterFullpdfviewPlugin.h>)
+#import <flutter_fullpdfview/FlutterFullpdfviewPlugin.h>
+#else
+@import flutter_fullpdfview;
+#endif
+
+#if __has_include(<image_picker/FLTImagePickerPlugin.h>)
+#import <image_picker/FLTImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<local_auth/FLTLocalAuthPlugin.h>)
+#import <local_auth/FLTLocalAuthPlugin.h>
+#else
+@import local_auth;
+#endif
+
+#if __has_include(<open_file/OpenFilePlugin.h>)
+#import <open_file/OpenFilePlugin.h>
+#else
+@import open_file;
+#endif
+
+#if __has_include(<package_info/FLTPackageInfoPlugin.h>)
+#import <package_info/FLTPackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<permission_handler/PermissionHandlerPlugin.h>)
+#import <permission_handler/PermissionHandlerPlugin.h>
+#else
+@import permission_handler;
+#endif
+
+#if __has_include(<printing/PrintingPlugin.h>)
+#import <printing/PrintingPlugin.h>
+#else
+@import printing;
+#endif
+
+#if __has_include(<storage_space/StorageSpacePlugin.h>)
+#import <storage_space/StorageSpacePlugin.h>
+#else
+@import storage_space;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SwiftDiskSpacePlugin registerWithRegistrar:[registry registrarForPlugin:@"SwiftDiskSpacePlugin"]];
+  [FilePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FilePickerPlugin"]];
+  [FlutterFullpdfviewPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterFullpdfviewPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTLocalAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTLocalAuthPlugin"]];
+  [OpenFilePlugin registerWithRegistrar:[registry registrarForPlugin:@"OpenFilePlugin"]];
+  [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [PrintingPlugin registerWithRegistrar:[registry registrarForPlugin:@"PrintingPlugin"]];
+  [StorageSpacePlugin registerWithRegistrar:[registry registrarForPlugin:@"StorageSpacePlugin"]];
+}
+
+@end
---
+++ pdf_reader/lib/generated_plugin_registrant.dart
@@ -0,0 +1,19 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:printing/printing_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  FilePickerWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PrintingPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
